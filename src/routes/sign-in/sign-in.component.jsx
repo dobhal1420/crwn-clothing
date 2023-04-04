b1048c5a2f62c5ce64f8567c36b15a102d10db1c
Ankit <ankit.dobhal@forto.com>
2023-04-04 17:19:15 +0200

    created sign up and Custom button

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,10 +1,13 @@
 import {signInWithGooglePopup, createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils'
+import SignUpForm from '../../components/sign-up-from/sign-up-form.component'
 
 const SignIn = () => {
+
 
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup()
         const userDocRef = await createUserDocumentFromAuth(user)
+
     }
 
     return (
@@ -13,6 +16,7 @@ const SignIn = () => {
             <button onClick={logGoogleUser}>
                 Sign in with Google Popup.
             </button>
+            <SignUpForm />
         </div>
     )
 }
