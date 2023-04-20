e429c0f884fc80eabc6705d5847900a3a8169a73
Ankit <ankit.dobhal@forto.com>
2023-04-20 17:58:16 +0200

    Adding context

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import { signInAuthUserWithEmailAndPassword, createUserDocumentFromAuth, signInWithGooglePopup } from '../../utils/firebase/firebase.utils'
 import FormInput from '../form-input/form-input.component'
 import Button from '../button/button.component'
 import './sign-in-form.styles.scss'
+import { UserContext } from '../../contexts/user.context'
 
 
 const defaultFormFields = {
@@ -15,7 +16,7 @@ const SignInForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields)
     const { email, password } = formFields
     
-    console.log(formFields)
+    const { setCurrentUser } = useContext(UserContext)
 
     const resetFormFields = () => {
         setFormFields(defaultFormFields)
@@ -31,8 +32,8 @@ const SignInForm = () => {
         
 
         try {
-            const response = await signInAuthUserWithEmailAndPassword(email, password)
-            console.log(response)
+            const { user } = await signInAuthUserWithEmailAndPassword(email, password)
+            setCurrentUser(user)
             resetFormFields()
         }
         catch (error) {
